f8a5d0ad6068974819591adf5c529adfc6512dff
carsenk <carsenk@gmail.com>
2017-12-06 12:54:50 -0700

    Protocol 21213

--- src/version.h
+++ src/version.h
@@ -24,23 +24,23 @@ extern const std::string CLIENT_DATE;
 //
 // database format versioning
 //
-static const int DATABASE_VERSION = 31213;
+static const int DATABASE_VERSION = 21213;
 
 //
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 31213; //Old Version 21213
+static const int PROTOCOL_VERSION = 21213; //Old Version 21213
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 21212;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 31213;
+static const int MIN_PEER_PROTO_VERSION = 21213;
 
-static const int MIN_INSTANTX_PROTO_VERSION = 31213;
+static const int MIN_INSTANTX_PROTO_VERSION = 21213;
 
-static const int MIN_MN_PROTO_VERSION = 31213;
+static const int MIN_MN_PROTO_VERSION = 21213;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
