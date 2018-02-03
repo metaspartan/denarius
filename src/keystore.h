084abf54e394c80e3457df9d88322fbb51c4e451
Thierry <thierry.ligeon@hva.nl>
2018-02-03 18:41:26 +0100

    Fixed Typo

--- src/keystore.h
+++ src/keystore.h
@@ -2,8 +2,8 @@
 // Copyright (c) 2009-2012 The Bitcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-#ifndef BITCOIN_KEYSDNRE_H
-#define BITCOIN_KEYSDNRE_H
+#ifndef BITCOIN_KEYSTORE_H
+#define BITCOIN_KEYSTORE_H
 
 #include "key.h"
 #include "crypter.h"
