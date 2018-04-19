baf69b59d4feb3d329363b7e2a9f69504600abdf
enkayz <lord.nk@gmail.com>
2018-04-19 23:22:19 +0930

    test for less rigid checkpoints

--- src/checkpoints.h
+++ src/checkpoints.h
@@ -51,8 +51,10 @@ namespace Checkpoints
     CBlockIndex* GetLastSyncCheckpoint();
     bool WriteSyncCheckpoint(const uint256& hashCheckpoint);
     bool AcceptPendingSyncCheckpoint();
-    uint256 AutoSelectSyncCheckpoint();
-    bool CheckSync(const uint256& hashBlock, const CBlockIndex* pindexPrev);
+    //uint256 AutoSelectSyncCheckpoint();
+    //bool CheckSync(const uint256& hashBlock, const CBlockIndex* pindexPrev);
+    const CBlockIndex* AutoSelectSyncCheckpoint();
+    bool CheckSync(int nHeight);
     bool WantedByPendingSyncCheckpoint(uint256 hashBlock);
     bool ResetSyncCheckpoint();
     void AskForPendingSyncCheckpoint(CNode* pfrom);
