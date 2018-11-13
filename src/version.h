69f596340e0b98dc15c7e22d033a00442afd5303
carsenk <carsenk@gmail.com>
2018-11-12 18:14:55 -0700

    Update Min Protocol

--- src/version.h
+++ src/version.h
@@ -36,10 +36,10 @@ static const int PROTOCOL_VERSION = 31000; //Protocol is now 31000 as of D v3.1.
 static const int INIT_PROTO_VERSION = 21212;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 31000;
+static const int MIN_PEER_PROTO_VERSION = 30000;
 
 // Don't forget to change proto in FORTUNA.h as well!!
-static const int MIN_MN_PROTO_VERSION = 31000;
+static const int MIN_MN_PROTO_VERSION = 30000;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
