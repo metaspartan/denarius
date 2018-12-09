9244b33131d87c69310b02207edb5bc52a33464b
enkayz <lord.nk@gmail.com>
2018-12-10 04:28:07 +1030

    code cleanup

--- src/fortunastake.cpp
+++ src/fortunastake.cpp
@@ -773,94 +773,10 @@ int CFortunaStake::GetPaymentAmount(const CBlockIndex *pindex, int nMaxBlocksToS
 
 }
 
-// loop back a full round
-// find the FS payment in each block
-// find the FS in our vector and add the payData
-
-bool UpdateFSPaymentAmounts(const CBlockIndex *pindex) {
-    const CBlockIndex *BlockReading = pindex;
-    int scanBack = max(FORTUNASTAKE_FAIR_PAYMENT_MINIMUM, (int)mnCount) * FORTUNASTAKE_FAIR_PAYMENT_ROUNDS;
-
-    CTxDB txdb;
-    int64_t amount;
-    CScript payee;
-
-    if (fDebug) { printf("UpdateFSPaymentAmounts(): Updating Fortunastake payments...");}
-    for (int i = 0; i < scanBack; i++) {
-            bool found = false;
-            CBlock block;
-            if(!block.ReadFromDisk(BlockReading, true)) // shouldn't really happen
-                continue;
-
-            MapPrevTx mapInputs;
-            map<uint256, CTxIndex> mapUnused;
-            bool fInvalid = false;
-            int64_t nFees;
-            if (!block.vtx[1].FetchInputs(txdb, mapUnused, false, false, mapInputs, fInvalid))
-            {
-                if (fInvalid)
-                    return error("UpdateFSPaymentAmounts(): FetchInputs found invalid tx");
-                return false;
-            }
-
-            nFees = block.vtx[1].GetValueIn(mapInputs) - block.vtx[1].GetValueOut();
-
-            if(block.IsProofOfStake() && pindexBest != NULL){
-                // Calculate Coin Age for Fortunastake Reward Calculation
-                uint64_t nCoinAge;
-                if (!block.vtx[1].GetCoinAge(txdb, nCoinAge))
-                    return error("UpdateFSPaymentAmounts(): %s unable to get coin age for coinstake, Can't Calculate Fortunastake Reward\n", block.vtx[1].GetHash().ToString().substr(0,10).c_str());
-                int64_t nCalculatedStakeReward = GetProofOfStakeReward(nCoinAge, nFees);
-                // Calculate expected fortunastakePaymentAmmount
-                amount = GetFortunastakePayment(BlockReading->nHeight, nCalculatedStakeReward);
-                for (unsigned int i = 0; i < block.vtx[1].vout.size(); i++) {
-                    //if(fDebug) { printf("CheckBlock-POS() : Payment vout number: %i , Amount: %ld\n",i, vtx[1].vout[i].nValue); }
-                    if(block.vtx[1].vout[i].nValue == amount )
-                    {
-                        payee = block.vtx[1].vout[i].scriptPubKey;
-                        found = true;
-                    }
-                }
-            }
-
-            if(block.IsProofOfWork() && pindexBest != NULL){
-                amount = GetFortunastakePayment(BlockReading->nHeight, block.vtx[0].GetValueOut());
-                for (unsigned int i = 0; i < block.vtx[0].vout.size(); i++) {
-                    //if(fDebug) { printf("CheckBlock-POW() : Payment vout number: %i , Amount: %lld\n",i, vtx[0].vout[i].nValue); }
-                    if(block.vtx[0].vout[i].nValue == amount)
-                    {
-                        payee = block.vtx[0].vout[i].scriptPubKey;
-                        found = true;
-                    }
-                }
-            }
-
-            if (found && amount) {
-                // search our list to update the payData
-                BOOST_FOREACH(CFortunaStake& mn, vecFortunastakes)
-                {
-                    CScript pubScript = GetScriptForDestination(mn.pubkey.GetID());
-                    if (payee == pubScript)
-                    {
-//                        CTxDestination address1;
-//                        ExtractDestination(pubScript, address1);
-//                        CBitcoinAddress address2(address1);
-                        mn.payData.push_back(make_pair(BlockReading->nHeight, amount));
-                        //if (fDebug) { printf("UpdateFSPaymentAmounts(): Block height %d payment data %s written for %s.",BlockReading->nHeight,FormatMoney(amount).c_str(), address2.ToString().c_str()); }
-                    }
-                }
-            }
-    }
-
-}
-
-// result vector = cscript, intr64_t e.g payment value
-// 2nd vector cscript, int e.g. payment count
 int CFortunaStake::UpdateLastPaidAmounts(const CBlockIndex *pindex, int nMaxBlocksToScanBack, int &value)
 {
     if (!pindex || IsInitialBlockDownload()) return 0;
     if (now > pindex->GetBlockTime()) return 0; // don't update paid amounts for nodes before the block they broadcasted on
-    UpdateFSPaymentAmounts(pindex);
     if (payData.size()) return 0; // let's only do the payData once, it is cleared if the chain is reorged
 
     const CBlockIndex *BlockReading = pindex;
@@ -938,84 +854,6 @@ int CFortunaStake::UpdateLastPaidAmounts(const CBlockIndex *pindex, int nMaxBloc
 
 }
 
-/*
-int CFortunaStake::UpdateLastPaidAmounts(const CBlockIndex *pindex, int nMaxBlocksToScanBack, int &value)
-{
-    if(!pindex) return 0;
-
-    const CBlockIndex *BlockReading = pindex;
-    int scanBack = max(FORTUNASTAKE_FAIR_PAYMENT_MINIMUM, (int)mnCount) * FORTUNASTAKE_FAIR_PAYMENT_ROUNDS;
-
-    CScript mnpayee = GetScriptForDestination(pubkey.GetID());
-    CTxDestination address1;
-    ExtractDestination(mnpayee, address1);
-    CBitcoinAddress address2(address1);
-    int rewardCount = 0;
-    int64_t rewardValue = 0;
-    int64_t val = 0;
-    value = 0;
-
-    // reset counts
-    payCount = 0;
-    payValue = 0; // vector of payValues w/ payHeight as index means we dont have to clear it, we can just remove any indexes which are < pindexBest->nHeight and rely on new payment data once we have done the initial search
-    // payData.clear();
-
-    // edit associated code in ConnectBlock() code to add <int,int> to the payData
-
-    LOCK(cs_fortunastakes);
-    if (!payData.size()) { // if we don't have payData, let's look it up!
-        for (int i = 0; i < scanBack; i++) {
-                val = 0;
-                CBlock block;
-                if(!block.ReadFromDisk(BlockReading, true)) // shouldn't really happen
-                    continue;
-
-                // if it's a legit block, then count it against this node and record it in a vector
-                if (block.IsProofOfWork() || block.IsProofOfStake())
-                {
-                    BOOST_FOREACH(CTxOut txout, block.vtx[block.IsProofOfWork() ? 0 : 1].vout)
-                    {
-                        if(mnpayee == txout.scriptPubKey) {
-                            int height = BlockReading->nHeight;
-                            if (rewardCount == 0) {
-                                nBlockLastPaid = height;
-                                nTimeLastPaid = BlockReading->nTime;
-                            }
-                            // values
-                            val = txout.nValue;
-
-                            // add this profit & the reward itself
-                            rewardValue += val;
-                            rewardCount++;
-
-                            // make a note in the node for later lookup
-                            payData.push_back(make_pair(height, val));
-                        }
-                    }
-                }
-
-            if (BlockReading->pprev == NULL) { assert(BlockReading); break; }
-
-            BlockReading = BlockReading->pprev;
-        }
-    }
-
-    // filter unneeded info from before scanBack height from payData,
-    // so we don't throw out data which might be needed if scanBack range gets bigger (e.g. huge new influx of stakes)
-    rewardCount = SetPayRate(pindexBest->nHeight);
-
-    if (rewardCount > 0)
-    {
-        value = payValue / COIN;
-        if (fDebug) printf("CFortunastake::UpdateLastPaidAmounts -- Processed stake %s (%d range) @ rate:%.2f count:%d val:%s\n", address2.ToString().c_str(), scanBack, payRate, payCount, FormatMoney(payValue).c_str());
-        return rewardCount;
-    } else {
-        value = 0;
-        return 0;
-    }
-
-}
-*/
 void CFortunaStake::UpdateLastPaidBlock(const CBlockIndex *pindex, int nMaxBlocksToScanBack)
 {
     if(!pindex) return;
