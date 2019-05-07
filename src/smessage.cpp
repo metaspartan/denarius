2379ea22aaa8626568bcb5f27011dbb4c69a7260
eureka <scv@brinstar.org>
2019-05-07 04:49:59 -0500

    Support both OpenSSL 1.0.x and 1.1.x interface

--- src/smessage.cpp
+++ src/smessage.cpp
@@ -126,15 +126,23 @@ bool SecMsgCrypter::Encrypt(unsigned char* chPlaintext, uint32_t nPlain, std::ve
     int nCLen = nLen + AES_BLOCK_SIZE, nFLen = 0;
     vchCiphertext = std::vector<unsigned char> (nCLen);
 
-    EVP_CIPHER_CTX *ctx;
-
     bool fOk = true;
 
+    #if OPENSSL_VERSION_NUMBER < 0x10100000L
+    EVP_CIPHER_CTX ctx;
+    EVP_CIPHER_CTX_init(&ctx);
+    if (fOk) fOk = EVP_EncryptInit_ex(&ctx, EVP_aes_256_cbc(), NULL, &chKey[0], &chIV[0]);
+    if (fOk) fOk = EVP_EncryptUpdate(&ctx, &vchCiphertext[0], &nCLen, chPlaintext, nLen);
+    if (fOk) fOk = EVP_EncryptFinal_ex(&ctx, (&vchCiphertext[0])+nCLen, &nFLen);
+    EVP_CIPHER_CTX_cleanup(&ctx);
+    #else
+    EVP_CIPHER_CTX *ctx;
     ctx = EVP_CIPHER_CTX_new();
     if (fOk) fOk = EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, &chKey[0], &chIV[0]);
     if (fOk) fOk = EVP_EncryptUpdate(ctx, &vchCiphertext[0], &nCLen, chPlaintext, nLen);
     if (fOk) fOk = EVP_EncryptFinal_ex(ctx, (&vchCiphertext[0])+nCLen, &nFLen);
     EVP_CIPHER_CTX_free(ctx);
+    #endif
 
     if (!fOk)
         return false;
@@ -154,15 +162,23 @@ bool SecMsgCrypter::Decrypt(unsigned char* chCiphertext, uint32_t nCipher, std::
     
     vchPlaintext.resize(nCipher);
 
-    EVP_CIPHER_CTX *ctx;
-
     bool fOk = true;
 
+    #if OPENSSL_VERSION_NUMBER < 0x10100000L
+    EVP_CIPHER_CTX ctx;
+    EVP_CIPHER_CTX_init(&ctx);
+    if (fOk) fOk = EVP_DecryptInit_ex(&ctx, EVP_aes_256_cbc(), NULL, &chKey[0], &chIV[0]);
+    if (fOk) fOk = EVP_DecryptUpdate(&ctx, &vchPlaintext[0], &nPLen, &chCiphertext[0], nCipher);
+    if (fOk) fOk = EVP_DecryptFinal_ex(&ctx, (&vchPlaintext[0])+nPLen, &nFLen);
+    EVP_CIPHER_CTX_cleanup(&ctx);
+    #else
+    EVP_CIPHER_CTX *ctx;
     ctx = EVP_CIPHER_CTX_new();
     if (fOk) fOk = EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, &chKey[0], &chIV[0]);
     if (fOk) fOk = EVP_DecryptUpdate(ctx, &vchPlaintext[0], &nPLen, &chCiphertext[0], nCipher);
     if (fOk) fOk = EVP_DecryptFinal_ex(ctx, (&vchPlaintext[0])+nPLen, &nFLen);
     EVP_CIPHER_CTX_free(ctx);
+    #endif
 
     if (!fOk)
         return false;
@@ -3138,15 +3154,30 @@ int SecureMsgValidate(unsigned char *pHeader, unsigned char *pPayload, uint32_t 
     for (int i = 0; i < 32; i+=4)
         memcpy(civ+i, &nonse, 4);
     
+
+    unsigned int nBytes;
+
+    #if OPENSSL_VERSION_NUMBER < 0x10100000L
+    HMAC_CTX ctx;
+    HMAC_CTX_init(&ctx);
+    #else 
     HMAC_CTX *ctx;
     ctx = HMAC_CTX_new();
-    
-    unsigned int nBytes;
+    #endif
+
+    #if OPENSSL_VERSION_NUMBER < 0x10100000L
+    if (!HMAC_Init_ex(&ctx, &civ[0], 32, EVP_sha256(), NULL)
+        || !HMAC_Update(&ctx, (unsigned char*) pHeader+4, SMSG_HDR_LEN-4)
+        || !HMAC_Update(&ctx, (unsigned char*) pPayload, nPayload)
+        || !HMAC_Update(&ctx, pPayload, nPayload)
+        || !HMAC_Final(&ctx, sha256Hash, &nBytes)
+    #else
     if (!HMAC_Init_ex(ctx, &civ[0], 32, EVP_sha256(), NULL)
         || !HMAC_Update(ctx, (unsigned char*) pHeader+4, SMSG_HDR_LEN-4)
         || !HMAC_Update(ctx, (unsigned char*) pPayload, nPayload)
         || !HMAC_Update(ctx, pPayload, nPayload)
         || !HMAC_Final(ctx, sha256Hash, &nBytes)
+    #endif
         || nBytes != 32)
     {
         if (fDebugSmsg)
@@ -3170,7 +3201,11 @@ int SecureMsgValidate(unsigned char *pHeader, unsigned char *pPayload, uint32_t 
             rv = 3; // checksum mismatch
         }
     }
+    #if OPENSSL_VERSION_NUMBER < 0x10100000L
+    HMAC_CTX_cleanup(&ctx);
+    #else
     HMAC_CTX_free(ctx);
+    #endif
     
     return rv;
 };
@@ -3546,18 +3581,35 @@ int SecureMsgEncrypt(SecureMessage& smsg, std::string& addressFrom, std::string&
     //    Message authentication code, (hash of timestamp + destination + payload)
     bool fHmacOk = true;
     unsigned int nBytes = 32;
+
+    #if OPENSSL_VERSION_NUMBER < 0x10100000L
+    HMAC_CTX ctx;
+    HMAC_CTX_init(&ctx);
+    #else
     HMAC_CTX *ctx;
     ctx = HMAC_CTX_new();
+    #endif
     
+    #if OPENSSL_VERSION_NUMBER < 0x10100000L
+    if (!HMAC_Init_ex(&ctx, &key_m[0], 32, EVP_sha256(), NULL)
+        || !HMAC_Update(&ctx, (unsigned char*) &smsg.timestamp, sizeof(smsg.timestamp))
+        || !HMAC_Update(&ctx, &vchCiphertext[0], vchCiphertext.size())
+        || !HMAC_Final(&ctx, smsg.mac, &nBytes)
+    #else
     if (!HMAC_Init_ex(ctx, &key_m[0], 32, EVP_sha256(), NULL)
         || !HMAC_Update(ctx, (unsigned char*) &smsg.timestamp, sizeof(smsg.timestamp))
         || !HMAC_Update(ctx, &vchCiphertext[0], vchCiphertext.size())
         || !HMAC_Final(ctx, smsg.mac, &nBytes)
+    #endif
         || nBytes != 32)
         fHmacOk = false;
-    
+
+    #if OPENSSL_VERSION_NUMBER < 0x10100000L
+    HMAC_CTX_cleanup(&ctx);
+    #else
     HMAC_CTX_free(ctx);
-    
+    #endif
+
     if (!fHmacOk)
     {
         printf("Could not generate MAC.\n");
@@ -3848,18 +3900,35 @@ int SecureMsgDecrypt(bool fTestOnly, std::string& address, unsigned char *pHeade
     unsigned char MAC[32];
     bool fHmacOk = true;
     unsigned int nBytes = 32;
+
+    #if OPENSSL_VERSION_NUMBER < 0x10100000L
+    HMAC_CTX ctx;
+    HMAC_CTX_init(&ctx);
+    #else
     HMAC_CTX *ctx;
     ctx = HMAC_CTX_new();
-    
+    #endif
+
+    #if OPENSSL_VERSION_NUMBER < 0x10100000L
+    if (!HMAC_Init_ex(&ctx, &key_m[0], 32, EVP_sha256(), NULL)
+        || !HMAC_Update(&ctx, (unsigned char*) &psmsg->timestamp, sizeof(psmsg->timestamp))
+        || !HMAC_Update(&ctx, pPayload, nPayload)
+        || !HMAC_Final(&ctx, MAC, &nBytes)
+    #else
     if (!HMAC_Init_ex(ctx, &key_m[0], 32, EVP_sha256(), NULL)
         || !HMAC_Update(ctx, (unsigned char*) &psmsg->timestamp, sizeof(psmsg->timestamp))
         || !HMAC_Update(ctx, pPayload, nPayload)
         || !HMAC_Final(ctx, MAC, &nBytes)
+    #endif
         || nBytes != 32)
         fHmacOk = false;
-    
+
+    #if OPENSSL_VERSION_NUMBER < 0x10100000L
+    HMAC_CTX_cleanup(&ctx);
+    #else    
     HMAC_CTX_free(ctx);
-    
+    #endif
+
     if (!fHmacOk)
     {
         printf("Could not generate MAC.\n");
