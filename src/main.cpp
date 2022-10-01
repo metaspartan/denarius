fac80b73e59d59a978c1ce40bd86952e95b16c11
Meta Spartan <carsenk@gmail.com>
2022-10-01 13:48:34 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2964,7 +2964,7 @@ bool CBlock::ConnectBlock(CTxDB& txdb, CBlockIndex* pindex, bool fJustCheck, boo
                                     paymentOK = true;
                                     break;
                                 } else if (payee == burnPayee) {
-                                    printf("CheckBlock-POW() : Found fortunastake payment: %s D to burn address.\n", FormatMoney(vtx[1].vout[i].nValue).c_str());
+                                    printf("CheckBlock-POW() : Found fortunastake payment: %s D to burn address.\n", FormatMoney(vtx[0].vout[i].nValue).c_str());
                                     foundPayee = true;
                                 }
                             }
